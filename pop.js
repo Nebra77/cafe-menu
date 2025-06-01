08f09a41909dcc7b626b5d68727d574c7a80ef27
Nebra77 <arbenhusenaj09@gmail.com>
2025-06-01 14:33:40 -0400

    Create pop.js

---
+++ pop.js
@@ -0,0 +1,53 @@
+function popPhobosPhilly() {
+  var popup = document.getElementById("PhobosPhilly");
+  popup.classList.toggle("show");
+}
+function popTerraformerTurkey() {
+  var popup = document.getElementById("TerraformerTurkey");
+  popup.classList.toggle("show");
+}
+
+function popFirestormBLT() {
+  var popup = document.getElementById("FirestormBLT");
+  popup.classList.toggle("show");
+}
+
+function popRedDustEspresso() {
+  var popup = document.getElementById("RedDustEspresso");
+  popup.classList.toggle("show");
+}
+
+function popSolarInfusedTeas() {
+  var popup = document.getElementById("SolarInfusedTeas");
+  popup.classList.toggle("show");
+}
+
+function popCryoCocoa() {
+  var popup = document.getElementById("CryoCocoa");
+  popup.classList.toggle("show");
+}
+
+function popLowGColdBrew() {
+  var popup = document.getElementById("LowGColdBrew");
+  popup.classList.toggle("show");
+}
+
+function popMarsMacaroons() {
+  var popup = document.getElementById("MarsMacaroons");
+  popup.classList.toggle("show");
+}
+
+function popChocolateLavaMooncakes() {
+  var popup = document.getElementById("ChocolateLavaMooncakes");
+  popup.classList.toggle("show");
+}
+
+function popCraterCroissants() {
+  var popup = document.getElementById("CraterCroissants");
+  popup.classList.toggle("show");
+}
+
+function popFreezeDriedFruitBites() {
+  var popup = document.getElementById("FreezeDriedFruitBites");
+  popup.classList.toggle("show");
+}
